a30220cb192a10beb8a743ebc53c27574178d996
Tanktiger <Tom.Scheduikat@gmail.com>
2013-08-05 09:25:44 +0200

    searcher edited
    Change-Id: I4ee316274a0826a6b90de61426c973f76daf3030

--- server/cards.php
+++ server/cards.php
@@ -4,8 +4,25 @@ class Cards
     public function getSingleCardByName($name) {
         $db = mysqli_connect('127.0.0.1', 'root', '', 'ygo');
 
-        $sql = 'SELECT name, url, language FROM cards WHERE name LIKE "%' . $name . '%"';
+        $sql = 'SELECT name, url, language FROM cards WHERE name LIKE "%' . $this->rip_tags($name) . '%"';
         $result = $db->query($sql);
         return $result->fetch_assoc();
+    }
+
+    private function rip_tags($string) {
+
+        // ----- remove HTML TAGs -----
+        $string = preg_replace ('/<[^>]*>/', ' ', $string);
+
+        // ----- remove control characters -----
+        $string = str_replace("\r", '', $string);    // --- replace with empty space
+        $string = str_replace("\n", ' ', $string);   // --- replace with space
+        $string = str_replace("\t", ' ', $string);   // --- replace with space
+
+        // ----- remove multiple spaces -----
+        $string = trim(preg_replace('/ {2,}/', ' ', $string));
+
+        return $string;
+
     }
 }
--- server/ygo_crawler.php
+++ server/ygo_crawler.php
@@ -9,81 +9,98 @@ $englishCardListUrl = array(
 		'http://yugioh.wikia.com/wiki/Category:English_Set_Card_Galleries?pagefrom=Phantom+Darkness+%28TCG-EN-1E%29#mw-pages',
 );
 
-$db = new mysqli('127.0.0.1', 'root', '1337', 'ygo');
+$db = new mysqli('127.0.0.1', 'root', '', 'ygo');
 $db->set_charset('utf8');
 
-echo 'Starte mit dem holen der Links für die Karten!' . PHP_EOL;
-$cardlinks = array();
-foreach ($englishCardListUrl as $url) {
-	echo '.';
-	$curlResult = setCurl($url);
-// 	file_put_contents('wiki'.'.html', $curlResult);
-	$dom = new DOMDocument('1.0', 'UTF-8');
-	@$dom->loadHTML($curlResult);
-	$cardlinkArray = getCategoryLinks($dom);
-	$cardlinks = array_merge($cardlinks, $cardlinkArray);
-	break;
+if (isset($_SERVER['argv'][1])) {
+    getCards($db);
+} else {
+    getPicLinks($englishCardListUrl, $db);
+    getCards($db);
 }
-echo 'Gefunden Links: ' . count($cardlinks) . PHP_EOL;
 
-echo 'Begin with getting the Pictures' . PHP_EOL;
-foreach ($cardlinks as $link) {
-	echo 'new Category: ' . $link . PHP_EOL;
-    $curlResult = setCurl($wikiUrl . $link);
-    @$dom->loadHTML($curlResult);
-    $div = $dom->getElementById('gallery-0');
-	if (isset($div)) {
-		$xpath = new DOMXPath($dom);
-		$spans = $xpath->query('//div[@id="gallery-0"]/span[@class="wikia-gallery-item"]');
-		$singleCardLinks = array();
-		for ($i = $spans->length - 1; $i > -1; $i--) {
-			$singleCardLinks[] = $wikiUrl . $spans->item($i)->lastChild->firstChild->getAttribute('href');
-		}
-		echo 'get Pics: ' . PHP_EOL;
-			//erstmal laden der Links und dann nen extra Crawler der die aus der DB holt??
-		foreach ($singleCardLinks as $singleCard) {
-			echo '.';
-			$curlResult = setCurl($singleCard);
-			@$dom->loadHTML($curlResult);
-			
-			$xpathCard = new DOMXPath($dom);
-			$trs = $xpathCard->query('//table[@class="cardtable"]/tr');
-			$cardValues = array();
-			foreach ($trs as $tr) {
-				$category = str_replace('&nbsp;', '', htmlentities(utf8_decode($tr->firstChild->nodeValue)));
-				$values = getCategoryValue($category, $tr);
-				if ($values) {
-					$cardValues[$values['category']] = $values['value'];
-				}
-			}
-			var_dump($cardValues);
-			exit();
-		}
-// 	    foreach ($div->getElementsByTagName('a') as $picThumb) {
-// 			
-// 	        $cardLink = $picThumb->getAttribute('href');
-// 	        preg_match('/\/wiki\/File\:(.*)/', $cardLink, $cardName);
-// 	        $curlResult = setCurl($wikiUrl . $cardLink);
-// 	        @$dom->loadHTML($curlResult);
-// 	        $xpath = new DOMXPath($dom);
-// 	        $imgUrl = $xpath->evaluate("string(//div[@class='fullMedia']/a/@href)");
-// 	        if (isset($cardName[1])) {
-// 	            $query = 'INSERT IGNORE INTO cards (name, url, language) VALUES ("'.$cardName[1].'","' . $imgUrl . '", "de")';
-// 	            file_put_contents('pics_wikia/'.$cardName[1], file_get_contents($imgUrl));
+function getPicLinks ($englishCardListUrl, $db) {
+    echo 'Starte mit dem holen der Links fuer die Karten!' . PHP_EOL;
+    $wikiUrl = 'http://yugioh.wikia.com';
+    $cardlinks = array();
+    foreach ($englishCardListUrl as $url) {
+    	echo '.';
+    	$curlResult = setCurl($url);
+    // 	file_put_contents('wiki'.'.html', $curlResult);
+    	$dom = new DOMDocument('1.0', 'UTF-8');
+    	@$dom->loadHTML($curlResult);
+    	$cardlinkArray = getCategoryLinks($dom);
+    	$cardlinks = array_merge($cardlinks, $cardlinkArray);
+    	break;
+    }
+    echo 'getSingleCardLinks' . PHP_EOL;
+    foreach ($cardlinks as $link) {
+        echo 'new Category: ' . $link . PHP_EOL;
+        $curlResult = setCurl($wikiUrl . $link);
+        @$dom->loadHTML($curlResult);
+        $div = $dom->getElementById('gallery-0');
+        if (isset($div)) {
+            $xpath = new DOMXPath($dom);
+            $spans = $xpath->query('//div[@id="gallery-0"]/span[@class="wikia-gallery-item"]');
+            $singleCardLinks = array();
+            for ($i = $spans->length - 1; $i > -1; $i--) {
+                echo '.';
+                $singleCardLinks = $wikiUrl . $spans->item($i)->lastChild->firstChild->getAttribute('href');
+                $query = 'INSERT IGNORE INTO cards (url) VALUES ("' . $singleCardLinks . '")';
+                $result = $db->query($query);
+            }
+        } else {
+    		$query = 'INSERT IGNORE INTO cards (name, url, language) VALUES ("'.$wikiUrl . $link.'","' . $wikiUrl . $link . '", "fail")';
+            $result = $db->query($query);
+    	}
+    }
+    echo 'Gespeicherte Links: ' . count($cardlinks) . PHP_EOL;
+}
+
+function getCards ($db) {
+    $query = 'SELECT url FROM cards';
+    $result = $db->query($query);
+    echo 'Begin with getting the Pictures' . PHP_EOL;
+    while ($link = $result->fetch_assoc()) {
+            echo '.';
+            $dom = new DOMDocument('1.0', 'UTF-8');
+            $curlResult = setCurl($link['url']);
+            @$dom->loadHTML($curlResult);
+
+            $xpathCard = new DOMXPath($dom);
+            $trs = $xpathCard->query('//table[@class="cardtable"]/tr');
+            $cardValues = array();
+            foreach ($trs as $tr) {
+                $category = str_replace('&nbsp;', '', htmlentities(utf8_decode($tr->firstChild->nodeValue)));
+                $values = getCategoryValue($category, $tr);
+                if ($values) {
+                    $cardValues[$values['category']] = $values['value'];
+                }
+            }
+            var_dump($cardValues);
+            exit();
+            // 	    foreach ($div->getElementsByTagName('a') as $picThumb) {
+            //
+            // 	        $cardLink = $picThumb->getAttribute('href');
+            // 	        preg_match('/\/wiki\/File\:(.*)/', $cardLink, $cardName);
+            // 	        $curlResult = setCurl($wikiUrl . $cardLink);
+            // 	        @$dom->loadHTML($curlResult);
+            // 	        $xpath = new DOMXPath($dom);
+            // 	        $imgUrl = $xpath->evaluate("string(//div[@class='fullMedia']/a/@href)");
+            // 	        if (isset($cardName[1])) {
+            // 	            $query = 'INSERT IGNORE INTO cards (name, url, language) VALUES ("'.$cardName[1].'","' . $imgUrl . '", "de")';
+            // 	            file_put_contents('pics_wikia/'.$cardName[1], file_get_contents($imgUrl));
 // 	        }
 // 	    }
-	} else {
-		$query = 'INSERT IGNORE INTO cards (name, url, language) VALUES ("'.$wikiUrl . $link.'","' . $wikiUrl . $link . '", "fail")';
-        $result = $db->query($query);
-	}
+    }
+    echo 'finish';
 }
-echo 'finish';
 
 function getCategoryValue($category, $tr) {
 	$values = array();
 	//sind dann die beschreibungen
 	if (strlen($category) > 255) {
-		
+
 	//Bild holen
 	} else if(strlen($category) == 0) {
 		$values['category'] = 'pic';
